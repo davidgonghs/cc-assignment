0a6a8caa505f13b9214432f8a92fe8a35fec8ed9
David Gong <90584914+davidgonghs@users.noreply.github.com>
2022-04-21 13:42:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "db-employee.czz0hfqvljiq.us-east-1.rds.amazonaws.com"
+customhost = "cc-asm.cge3padjnblm.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "12345678"
 customdb = "employee"
-custombucket = "david-jeremy-jaedon-bucket"
+custombucket = "david-jeremy-jaedon-cc-asm-bucket"
 customregion = "us-east-1"
